f2b44b7e3d04b26027dc56a09f69f85f9ceda2fd
mmiah00 <mmiah00@stuty.edu>
2019-02-13 14:20:30 -0500

    wrote toSTring and fixed constructor

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,9 +1,10 @@
 public class KnightBoard {
   int[][] board;
-  int startx,starty;
   public KnightBoard (int startingRows, int startingCols) {
-    startx = startingCols;
-    starty = startingRows;
+    if (startingCols <= 0 || startingRows <= 0) {
+      throw new IllegalArgumentException ();
+    }
+    board = new int [startingRows][startingCols];
     reset ();
   }
 
@@ -14,4 +15,27 @@ public class KnightBoard {
       }
     }
   }
+
+  public String toString () {
+    String ans = "";
+    for (int y = 0; y < board.length; y ++) {
+      for (int x = 0; x < board[y].length; x ++) {
+        if (board[y][x] < 10) {
+          if (x == board[y].length - 1) {
+            ans += " " + x + "\n";
+          }
+          ans += " " + x + " ";
+        }
+        else {
+          if (x == board[y].length - 1) {
+            ans += x + "\n";
+          }
+          ans += x + " ";
+        }
+      }
+    }
+    return ans; 
+  }
+
+
 }
