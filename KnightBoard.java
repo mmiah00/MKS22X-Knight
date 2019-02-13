a69677478e1c60288e03c063d282bfeb3c601f4b
mmiah <mmiah00@stuy.edu>
2019-02-12 22:13:35 -0500

    constructor

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,3 +1,17 @@
 public class KnightBoard {
+  int[][] board;
+  int startx,starty;
+  public KnightBoard (int startingRows, int startingCols) {
+    startx = startingCols;
+    starty = startingRows;
+    reset ();
+  }
 
+  public void reset () {
+    for (int y = 0; y < board.length; y ++) {
+      for (int x = 0; x < board[y].length; x ++) {
+        board[y][x] = 0;
+      }
+    }
+  }
 }
