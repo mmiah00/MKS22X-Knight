5e45b0a32d0e2c5f61e9de16f8565e7d42444fc4
mmiah <mmiah00@stuy.edu>
2019-02-19 13:05:39 -0500

    solve doesn't work

--- Driver.java
+++ Driver.java
@@ -1,8 +1,10 @@
 public class Driver {
   public static void main (String[] args) {
     KnightBoard testing = new KnightBoard (5,5);
-    System.out.println (testing.solve (1,2));
-    //System.out.println (testing.toString ());
-
+    for (int y = 0; y < 5; y ++) {
+      for (int x = 0; x < 5; x ++) {
+        System.out.println (testing.solve (y,x)); 
+      }
+    }
   }
 }
